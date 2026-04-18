ecfbc32a0aa39ad5f8e437dc27f3a3fd7c3917d1
Kolen Cheung <christian.kolen@gmail.com>
2026-04-18 20:49:51 +0000

    Add echo banners to sbatch_monte_carlo_pi_mpi_hybrid_c.sh

--- src/section_05_python_array_jobs_parallelism_strategies/monte_carlo_pi_c/sbatch_monte_carlo_pi_mpi_hybrid_c.sh
+++ src/section_05_python_array_jobs_parallelism_strategies/monte_carlo_pi_c/sbatch_monte_carlo_pi_mpi_hybrid_c.sh
@@ -13,18 +13,33 @@ export OMP_DYNAMIC=FALSE
 module reset
 module load PrgEnv-gnu
 
+echo '=== N_PROC=1, N_THREADS=144 ==='
 OMP_NUM_THREADS=144 command time -v mpirun -n 1 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=2, N_THREADS=72 ==='
 OMP_NUM_THREADS=72 command time -v mpirun -n 2 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=3, N_THREADS=48 ==='
 OMP_NUM_THREADS=48 command time -v mpirun -n 3 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=4, N_THREADS=36 ==='
 OMP_NUM_THREADS=36 command time -v mpirun -n 4 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=6, N_THREADS=24 ==='
 OMP_NUM_THREADS=24 command time -v mpirun -n 6 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=8, N_THREADS=18 ==='
 OMP_NUM_THREADS=18 command time -v mpirun -n 8 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=9, N_THREADS=16 ==='
 OMP_NUM_THREADS=16 command time -v mpirun -n 9 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=12, N_THREADS=12 ==='
 OMP_NUM_THREADS=12 command time -v mpirun -n 12 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=16, N_THREADS=9 ==='
 OMP_NUM_THREADS=9 command time -v mpirun -n 16 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=18, N_THREADS=8 ==='
 OMP_NUM_THREADS=8 command time -v mpirun -n 18 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=24, N_THREADS=6 ==='
 OMP_NUM_THREADS=6 command time -v mpirun -n 24 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=36, N_THREADS=4 ==='
 OMP_NUM_THREADS=4 command time -v mpirun -n 36 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=48, N_THREADS=3 ==='
 OMP_NUM_THREADS=3 command time -v mpirun -n 48 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=72, N_THREADS=2 ==='
 OMP_NUM_THREADS=2 command time -v mpirun -n 72 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
+echo '=== N_PROC=144, N_THREADS=1 ==='
 OMP_NUM_THREADS=1 command time -v mpirun -n 144 monte_carlo_pi_mpi_hybrid -d 2 -n ${N}
