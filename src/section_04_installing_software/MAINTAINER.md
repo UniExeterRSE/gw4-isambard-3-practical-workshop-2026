9c536c1ff9b3b12269cb1d60e310a7db3085ef33
Kolen Cheung <christian.kolen@gmail.com>
2026-04-20 21:17:33 +0100

    add maintainer's notes

---
+++ src/section_04_installing_software/MAINTAINER.md
@@ -0,0 +1,9 @@
+# Instructions to create a fallback stack
+
+In case anyone cannot follow the instructions to set it up fully.
+
+Bootstrap everything:
+
+``` sh
+./maintainer.sh
+```
---
+++ src/section_04_installing_software/maintainer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# need these for bootstrap
+# __OPT_ROOT
+# MAMBA_ROOT_PREFIX
+# XDG_CONFIG_HOME
+
+# follow .bashrc logic but point to PROJECTDIR
+read -r __OSTYPE __ARCH <<< "$(uname -sm)"
+export __OSTYPE __ARCH
+# __LOCAL_ROOT <- arch-indep software prefix
+export __LOCAL_ROOT="${PROJECTDIR}/local"
+# __OPT_ROOT <- arch-dep software prefix
+export __OPT_ROOT="${__LOCAL_ROOT}/opt/${__OSTYPE}-${__ARCH}"
+export MAMBA_ROOT_PREFIX="${__OPT_ROOT}/miniforge3"
+export XDG_CONFIG_HOME="${PROJECTDIR}/local/config"
+
+cd "${DIR}/../../bootstrap"
+install/bootstrap.sh
+
+mamba env create -f "${DIR}/../../environment_hpc.yml" -p "${__OPT_ROOT}/isambard3-workshop-hpc" -y
+echo mamba activate "${__OPT_ROOT}/isambard3-workshop-hpc"
